f73631aefa17b0df9ccd89e8c1bf4af14876b59f
O <o@OdeMac-mini.local>
2026-03-25 20:48:42 +0800

    fix: exhaustive open-next config for 2026 build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,13 @@
-const config = {
+export default {
   default: {
-    minify: true,
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
   },
+  edgeExternals: ["node:crypto"],
 };
-
-export default config;
