c00dba26294baf8365ca8c182999bda04d7de26f
O <o@OdeMac-mini.local>
2026-03-25 20:50:07 +0800

    fix: match exhaustive open-next config schema

--- open-next.config.ts
+++ open-next.config.ts
@@ -10,4 +10,15 @@ export default {
     },
   },
   edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 };
